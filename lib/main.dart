d87cf180c159cc9c87a42762389fef103f3d05cc
whitecity01 <kh081104@gmail.com>
2024-06-10 00:33:42 +0900

    feat: env 로드 및 초기화 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:kakao_map_plugin/kakao_map_plugin.dart';
 import 'package:yu_lost_item/config/api.dart';
 import 'package:yu_lost_item/screens/login_page.dart';
 
-void main() {
+void main() async {
   getEnv();
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: "assets/env/.env");
+  AuthRepository.initialize(appKey: dotenv.env['APP_KEY'] ?? '');
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
